29a1f0977337a305e2b9d5aae6d0d0f96421db1c
Sébastien Lavoie <github@lavoie.sl>
2012-03-07 10:39:01 -0500

    Fixed bug where default_settings was being modified (Pull request #1)

--- jQuery.hoverable.js
+++ jQuery.hoverable.js
@@ -25,7 +25,7 @@
 
   var methods = {
     init: function(options) {
-      var settings = $.extend(default_settings, options);
+      var settings = $.extend({}, default_settings, options);
       var data = {
         settings: settings
       };
